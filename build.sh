687bc8921056f0c8545ffb317a4487a84e99b536
flitrue <812863096@qq.com>
2020-05-21 18:30:33 +0800

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd masterx/
 echo '打包主应用'
-npm install
-npm run build
+npm install && npm run build
 
 cd ../micro-vue/
 echo '打包微应用'
-npm install
-npm run build
+npm install && npm run build
